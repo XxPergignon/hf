16d6ea2596267603ed6ae148c6ea6c19cabb2078
XxPergignon <58432129+XxPergignon@users.noreply.github.com>
2023-01-03 20:49:56 +0000

    Adding light

--- main.js
+++ main.js
@@ -8,10 +8,12 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize( window.innerWidth, window.innerHeight );
 camera.position.setZ(30);
 const geometry =  new THREE.TorusGeometry( 10, 3, 16, 100 )
-const material = new THREE.MeshBasicMaterial( {
-  color: 0xFF6347,
-  wireframe: true
+const material = new THREE.MeshStandardMaterial( {
+  color: 0xFF6347
 });
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(20, 20, 20)
+
 const torus = new THREE.Mesh( geometry, material);
 function animate() {
   requestAnimationFrame( animate );
